88fd8742272c92369dfdef7f9ce982b9ae0d0dcb
wudibin <781721794@qq.com>
2020-10-23 10:59:17 +0800

    [wu] font : add Foot Component

---
+++ front-end/src/components/Foot.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import '../styles/Foot.css';
+
+class Foot extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="foot"/>
+        );
+    }
+}
+
+export default Foot;
